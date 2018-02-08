62b5e281afa3c5f738cf5046bd97cfd6cbda0eb4
Alexander Burchardt <aburchardt@emo.kea.local>
2018-02-08 11:29:29 +0100

    torsdag 1129

--- themes/aeroe/static/js/script.js
+++ themes/aeroe/static/js/script.js
@@ -1,16 +1,20 @@
+console.log("Godmorgen");
+
+window.addEventListener("load", sideLoadet);
+
+function sideLoadet() {
+  TweenMax.staggerFrom(".column_and_name_container p", 1, {opacity:"0"},-.1);
+  TweenMax.staggerFrom(".column_and_name_container svg", 1, {width:"0"},.1);
+
+}
+
 function openNav() {
     document.getElementById("mySidenav").style.left = "0";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.left = "-150px";
+    document.getElementById("mySidenav").style.left = "-170px";
 }
-
-function preloader() {
-  document.getElementById("preloader").style.display = "none";
-}
-
-setTimeout(function() { preloader(); }, 3000);
 
 /*Ajax contact form*/
 $("#kontaktmig").submit(function(event) {
