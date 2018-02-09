318650604f07aeb1d0f7a2d471e79a9624d2ec35
Alexander Burchardt <aburchardt@ahmed.kea.local>
2018-02-09 08:56:43 +0100

    fredag start

--- themes/aeroe/static/js/script.js
+++ themes/aeroe/static/js/script.js
@@ -3,9 +3,8 @@ console.log("Godmorgen");
 window.addEventListener("load", sideLoadet);
 
 function sideLoadet() {
-  TweenMax.staggerFrom(".column_and_name_container p", 1, {opacity:"0"},-.1);
-  TweenMax.staggerFrom(".column_and_name_container svg", 1, {width:"0"},.1);
-
+  TweenMax.staggerFrom(".column_and_name_container p", 2, {opacity:"0"},-.4);
+  TweenMax.staggerFrom(".column_and_name_container svg", 2, {width:"0"},.4);
 }
 
 function openNav() {
